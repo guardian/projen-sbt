ef542627cb7f48783de8efc685a7260159113918
Akash Askoolum <akash1810@gmail.com>
2021-07-23 14:04:10 +0100

    fix: Set a default sbt version

--- src/index.ts
+++ src/index.ts
@@ -2,14 +2,16 @@ import * as pj from 'projen';
 import { ProjectOptions } from 'projen/lib/project';
 
 export interface SbtProjectOptions extends ProjectOptions {
-  readonly sbtVersion: string;
+  readonly sbtVersion?: string;
 }
+
+const defaultSbtVersion = '1.5.5';
 
 export class SbtProject extends pj.Project {
   constructor(options: SbtProjectOptions) {
     super(options);
 
     const buildProperties = new pj.SourceCode(this, 'project/build.properties');
-    buildProperties.line(`sbt.version=${options.sbtVersion}`);
+    buildProperties.line(`sbt.version=${options.sbtVersion ?? defaultSbtVersion}`);
   }
 }
