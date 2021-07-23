27c9b1fdaa99330886f1eac779ace5eee855de0e
Akash Askoolum <akash1810@gmail.com>
2021-07-23 11:51:19 +0100

    set node version and create .nvmrc file

--- .projenrc.js
+++ .projenrc.js
@@ -1,16 +1,27 @@
-const { JsiiProject } = require('projen');
+const { JsiiProject, SourceCode } = require('projen');
+
+const projenDep = 'projen@0.27.6';
+const nodeLTSVersion = '14.17.3';
+
 const project = new JsiiProject({
   author: 'The Guardian',
   authorAddress: 'devx@theguardian.com',
   defaultReleaseBranch: 'main',
   name: '@guardian/projen-sbt',
   repositoryUrl: 'https://github.com/guardian/projen-sbt.git',
-
-  // deps: [],                          /* Runtime dependencies of this module. */
+  minNodeVersion: nodeLTSVersion,
+  maxNodeVersion: nodeLTSVersion,
+  workflowNodeVersion: nodeLTSVersion,
+  deps: [projenDep],
+  peerDeps: [projenDep],
   // description: undefined,            /* The description is just a string that helps people understand the purpose of the package. */
   // devDeps: [],                       /* Build dependencies for this module. */
   // packageName: undefined,            /* The "name" in package.json. */
   // projectType: ProjectType.UNKNOWN,  /* Which type of project this is (library/app). */
   // release: undefined,                /* Add release management to this project. */
 });
+
+const nvmrc = new SourceCode(project, '.nvmrc');
+nvmrc.line(nodeLTSVersion);
+
 project.synth();
