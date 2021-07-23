f6d62379ac50ed0d89fc0d23eeeff2f3024cf78d
Akash Askoolum <akash1810@gmail.com>
2021-07-23 14:28:23 +0100

    chore: move to .projenrc.ts

--- .projenrc.ts
+++ .projenrc.ts
@@ -1,12 +1,20 @@
-import { JsiiProject } from 'projen';
-const project = new JsiiProject({
-  author: 'Akash Askoolum',
-  authorAddress: 'akash1810@gmail.com',
-  defaultReleaseBranch: 'main',
-  name: 'projen-sbt',
-  projenrcTs: true,
-  repositoryUrl: 'git@github.com:guardian/projen-sbt.git',
+import { JsiiProject, SourceCode } from 'projen';
 
+const nodeLTSVersion = '14.17.3';
+const projenDep = 'projen@0.27.6';
+
+const project = new JsiiProject({
+  projenrcTs: true,
+  author: 'The Guardian',
+  authorAddress: 'devx@theguardian.com',
+  defaultReleaseBranch: 'main',
+  name: '@guardian/projen-sbt',
+  repositoryUrl: 'https://github.com/guardian/projen-sbt.git',
+  minNodeVersion: nodeLTSVersion,
+  maxNodeVersion: nodeLTSVersion,
+  workflowNodeVersion: nodeLTSVersion,
+  deps: [projenDep],
+  peerDeps: [projenDep],
   // deps: [],                          /* Runtime dependencies of this module. */
   // description: undefined,            /* The description is just a string that helps people understand the purpose of the package. */
   // devDeps: [],                       /* Build dependencies for this module. */
@@ -14,4 +22,8 @@ const project = new JsiiProject({
   // projectType: ProjectType.UNKNOWN,  /* Which type of project this is (library/app). */
   // release: undefined,                /* Add release management to this project. */
 });
+
+const nvmrc = new SourceCode(project, '.nvmrc');
+nvmrc.line(nodeLTSVersion);
+
 project.synth();
