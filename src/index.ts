0463e90850e3bdc02afe33cc3e51791ac6c6260b
Akash Askoolum <akash1810@gmail.com>
2021-07-23 14:47:40 +0100

    feat: Create `.projenrc.js` file through project

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,11 @@
 import * as pj from 'projen';
+import { Projenrc, ProjenrcOptions } from 'projen/lib/javascript';
 import { ProjectOptions } from 'projen/lib/project';
 
 export interface SbtProjectOptions extends ProjectOptions {
   readonly sbtVersion?: string;
+  readonly projenrcJs?: boolean;
+  readonly projenrcJsOptions?: ProjenrcOptions;
 }
 
 const defaultSbtVersion = '1.5.5';
@@ -13,5 +16,10 @@ export class SbtProject extends pj.Project {
 
     const buildProperties = new pj.SourceCode(this, 'project/build.properties');
     buildProperties.line(`sbt.version=${options.sbtVersion ?? defaultSbtVersion}`);
+
+    const projenrcJs = options.projenrcJs ?? true;
+    if (projenrcJs) {
+      new Projenrc(this, options.projenrcJsOptions);
+    }
   }
 }
